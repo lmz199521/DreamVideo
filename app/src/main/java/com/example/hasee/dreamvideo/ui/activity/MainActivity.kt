e9beb357264b9c12da08054322a19bdeb45a7d4f
lmz199521 <15936319282@163.com>
2019-06-06 16:57:26 +0800

    kotlin影音项目准备

---
+++ app/src/main/java/com/example/hasee/dreamvideo/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hasee.dreamvideo.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hasee.dreamvideo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
